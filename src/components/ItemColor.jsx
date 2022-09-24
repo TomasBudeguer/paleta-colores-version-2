642330e5e4c09624fd2f4a59685723f941291dfa
Tomas <tomibudeguer20@gmail.com>
2022-09-23 23:59:50 -0300

    agregue el item color creando una card con cada color

--- src/components/ItemColor.jsx
+++ src/components/ItemColor.jsx
@@ -1,9 +1,23 @@
-const ItemColor = () => {
-    return (
-        <div>
-            aca el color
+import { Card, Button } from "react-bootstrap";
+
+const ItemColor = (props) => {
+  return (
+    <div className="col-sm-12 col-md-4 col-lg-3 my-4">
+    <Card>
+      <Card.Body>
+        <Card.Title>{props.nombreColor}</Card.Title>
+        <hr />
+        <Card.Text className="text-center">
+        {props.nombreColor}
+        <hr />
+        </Card.Text>
+        <div className="d-flex justify-content-end">
+        <Button variant="danger">Borrar</Button>
         </div>
-    );
+      </Card.Body>
+    </Card>
+    </div>
+  );
 };
 
 export default ItemColor;
